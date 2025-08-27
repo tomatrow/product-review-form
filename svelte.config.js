5f3a5ae18d52ca74258b1c03c8582f801e02cf5c
AJ Caldwell <tomatrow@gmail.com>
2025-08-26 21:27:58 -0700

    Setup embedded app

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static"
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
-};
+	kit: {
+		adapter: adapter(),
+		embedded: true,
+		output: { bundleStrategy: "inline" },
+		router: { type: "hash" },
+		inlineStyleThreshold: 100
+	}
+}
 
-export default config;
+export default config
